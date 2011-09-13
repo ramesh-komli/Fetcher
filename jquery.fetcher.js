99fa4ba7ff20615f582ec22e57ccf3e1fb4812f3
Mike Smotherman <mike@codeinfused.com>
2011-09-13 15:08:02 -0400

    Fixed editor tab setting

--- jquery.fetcher.js
+++ jquery.fetcher.js
@@ -16,56 +16,56 @@
 */
 
 ;(function($){
-    
-    $.fetcher = function(url, name, cb){
-    	
-    	var def = $.Deferred(), defarr = [];
-    	
-    	if($.isArray(url)){
-    	
-    		for(var i=0; i<url.length; i++){
-    			defarr.push( $.fetcher.f(url[i][0], url[i][1], url[i][2]) );	
-    		};
-    	
-    	}else{
-    		defarr.push( $.fetcher.f(url, name, cb) );
-    	}
-    	
-    	$.when.apply($, defarr).then(def.resolve, def.reject);
-    	
-    	return def;
-    };
-    
-    $.fetcher.f = function(url, name, cb){
-    	var selector,
-    		matches,
-    		cb = (cb ? cb : $.isFunction(name) ? name : function(){}),
-    		name = typeof name==="string" ? name : ''
-    	;
-    	
-    	// get the filename without extension, and the url without the selector, and the selector
-    	if(name===''){
-    		matches = /^([\S]+)((\s[\S]+)*)$/.exec(url); // better way?
-    		url = matches[1];
-    		if(matches[3]){
-    			// remove selector prefixes for template name
-    			name = matches[3].replace(/[\#\.\s]/g, "");
-    			selector = matches[2].substr(1);
-    		}else{
-    			matches = /((^[\S]+\/)|^)(([\S]+)\.[\S]+)$/.exec(url); // better way?
-    			name = matches[4];
-    		}
-    	};
-    			
-    	return $.get(url, function(r){
-    		
-    		if(selector){
-    			$(r).find(selector).template(name);
-    		}else{
-    			$.template(name, r);
-    		};
-    		cb();
-    	});
-    };
+	
+	$.fetcher = function(url, name, cb){
+	
+		var def = $.Deferred(), defarr = [];
+		
+		if($.isArray(url)){
+		
+			for(var i=0; i<url.length; i++){
+				defarr.push( $.fetcher.f(url[i][0], url[i][1], url[i][2]) );	
+			};
+		
+		}else{
+			defarr.push( $.fetcher.f(url, name, cb) );
+		}
+		
+		$.when.apply($, defarr).then(def.resolve, def.reject);
+		
+		return def;
+	};
+	
+	$.fetcher.f = function(url, name, cb){
+		var selector,
+			matches,
+			cb = (cb ? cb : $.isFunction(name) ? name : function(){}),
+			name = typeof name==="string" ? name : ''
+		;
+		
+		// get the filename without extension, and the url without the selector, and the selector
+		if(name===''){
+			matches = /^([\S]+)((\s[\S]+)*)$/.exec(url); // better way?
+			url = matches[1];
+			if(matches[3]){
+				// remove selector prefixes for template name
+				name = matches[3].replace(/[\#\.\s]/g, "");
+				selector = matches[2].substr(1);
+			}else{
+				matches = /((^[\S]+\/)|^)(([\S]+)\.[\S]+)$/.exec(url); // better way?
+				name = matches[4];
+			}
+		};
+		
+		return $.get(url, function(r){
+		
+		if(selector){
+			$(r).find(selector).template(name);
+		}else{
+			$.template(name, r);
+		};
+			cb();
+		});
+	};
 
 })(jQuery);
